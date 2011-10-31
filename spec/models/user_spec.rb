5ba4f6f5d288395e21aa087d4ba7940e2b2af745
Ashwini Anand <ash@investopresto.com>
2011-11-01 00:54:46 +0530

    2nd commit of chapter 7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,5 +60,25 @@ describe User do
 		hash = @attr.merge(:password => long, :password_confirmation => long)
 		User.new(hash).should_not be_valid
 	end
+	
+	 describe "authenticate method" do
+
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+
+      it "should return nil for an email address with no user" do
+        nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+        nonexistent_user.should be_nil
+      end
+
+      it "should return the user on email/password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
+    end
+	  
+	
 end
  
