7eebaf1233c437f750e4dda3fcafc30ad6fc7da0
Ashwini Anand <ash@investopresto.com>
2011-11-08 04:45:15 +0530

    1st checkin on chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit,:update]
+  before_filter :correct_user, :only => [:edit,:update]
+
 	def new
     @user = User.new
 		@title = "Sign up"
@@ -19,5 +22,43 @@ class UsersController < ApplicationController
 			@title = "Sign up"
 			render 'new'
 		end
-	end
+  end
+
+  def edit
+    @user= User.find(params[:id])
+    @title="Edit User"
+  end
+
+  def update
+    @user= User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:sucess]= "Profile updated"
+      redirect_to @user
+    else
+      @title="Edit User"
+      render 'edit'
+    end
+  end
+
+  def index
+    @title = "All users"
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def correct_user
+    deny_access unless current_user == @user
+  end
+
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
   def current_user=(user)
 		 @current_user ||= user_from_remember_token
 	end
-	
+
 	def signed_in?
 		!current_user.nil?
 	end
@@ -21,8 +21,22 @@ module SessionsHelper
   def current_user
     @current_user ||= user_from_remember_token
   end
-	
+
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default )
+    clear_return_to
+  end
+
 	private
+
+    def store_location
+      session[:return_to] =request.fullpath
+    end
 		
 		def user_from_remember_token
 			User.authenticate_with_salt(*remember_token)
@@ -30,6 +44,10 @@ module SessionsHelper
 		
 		def remember_token
 			cookies.signed[:remember_token] || [nil,nil]
-		end
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
 		
 end
